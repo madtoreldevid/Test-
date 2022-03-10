a1bf2fd20badc98ec35fb63200fe7fef1af4288c
martoreldevid@gmail.com <education_20@dom01.internal>
2022-03-10 17:15:18 +0100

    testcomit

--- Modules/SimCorp.XMGR.clientX/SimCorp.XMGR.clientX.psm1
+++
@@ -1,9 +0,0 @@
-Write-Host "Loading module: SimCorp.XMGR.clientX" -ForegroundColor green
-
-function Get-XMGRHelloWorld{
-    SimCorp.XMGR\Get-XMGRHelloWorld
-    write-host "... also from the client"
-} 
-
-
-Export-ModuleMember -Function '*'
